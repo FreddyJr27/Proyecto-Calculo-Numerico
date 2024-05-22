2660f5db360908fc0984ceb30a39995a81037435
FreddyJr27 <freddyjr200427@gmail.com>
2024-05-22 15:55:11 -0400

    Se empezo el gauss seidel

--- proyecto 1 calculo numerico/gauss-seidel.py
+++ proyecto 1 calculo numerico/gauss-seidel.py
@@ -1,15 +1,51 @@
 import random
 import numpy as np
-n = int(input("Ingrese la dimensión de la matriz cuadrada (n): "))
+
+
+class GaussSeidelSolver:
+    def __init__(self, iteraciones=100, t=1e-6):
+        
+        self.ite = iteraciones
+        self.t = t
+
+    def solve(self, A, b):
+       
+        n = len(b)
+        #se crea un vector lleno de ceros
+        x = np.zeros(n)  
+
+        for _ in range(self.ite):
+            # se copia el vector x cada vez que se repite las iteraciones
+            x1 = np.copy(x)  
+
+            for i in range(n):
+                # Calcula la suma de los productos de los coeficientes y las soluciones conocidas,
+                # tanto para los términos anteriores como para los términos posteriores a la incógnita actual.
+                sum1 = np.dot(A[i, :i], x1[:i])
+                sum2 = np.dot(A[i, i + 1:], x[i + 1:])
+
+                # Calcula la nueva solución para la incógnita actual utilizando la fórmula del método de Gauss-Seidel.
+                x1[i] = (b[i] - sum1 - sum2) / A[i, i]
+
+            # Comprueba la condición de convergencia: si la norma de la diferencia entre las soluciones
+            # anteriores y las soluciones actuales es menor que la tolerancia, devuelve la solución actual.
+            if np.linalg.norm(x1 - x) < self.t:
+                return x1
+
+            x = x1  
+
+        return x
+#se le pide al usuario de cuanto va a ser la matriz nxn y se le pregunta si se va rellenar auto matico o no
+n = int(input("Cual va a ser el valor de n para la matriz nxn (n): "))
 p = input("Desea rellenar la matriz (r) o que se rellene automaticamente (a)?  ")
-# se creo una matriz de tamaño nxn
+#se crean las matrices llenas de ceros
 matriz = np.zeros((n,n))
 ma = np.zeros((n,1))
+#si el usuario quizo rellenar la matriz
 if p == "r":
-    # Crear una matriz vacía de tamaño nxn
     
-
-    # Pedir al usuario los valores para cada posición
+#se le pide con un bucle que vaya rellenando posicion por posicion 
+    
     for i in range(n):
         
         for j in range(n):
@@ -18,8 +54,9 @@ if p == "r":
     for k in range(n):
         valor2 = int(input(f"ingrese los valores de la segunda matriz ({k}): "))
         ma[k] = valor2
-
+#si el usuario quizo que se rellenara automatico
 elif p == "a":
+   #se rellena automatico con numeros de -100 a 100 mediante bucles
    for i in range(n):
         
         for j in range(n):
@@ -30,9 +67,14 @@ elif p == "a":
         valor2 = int(random.randint(-100,100))
         ma[k]= valor2
 
-# Imprimir la matriz ingresada por el usuario
-print("Matriz ingresada:")
+
+print("Matriz creada:")
 
 print(matriz)
 
 print(ma)
+
+solucion = GaussSeidelSolver()
+solucion1 = solucion.solve(matriz, ma)
+print("Solución:")
+print(solucion1)
